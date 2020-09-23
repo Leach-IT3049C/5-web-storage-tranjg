292393a7c94c8ef6cc884675f73630dde0d7fe51
tranjg <tranjg@mail.uc.edu>
2020-09-22 21:17:48 -0400

    implemented item add functionality

--- resources/js/index.js
+++ resources/js/index.js
@@ -5,3 +5,29 @@ const addBtn = document.getElementById(`add`);
 
 let itemsArray = localStorage.getItem(`items`) ? JSON.parse(localStorage.getItem(`items`)) : [] ;
 
+localStorage.setItem(`items`, JSON.stringify(itemsArray));
+const data = JSON.parse(localStorage.getItem(`items`));
+
+const liMaker = (text) => {
+    const li = document.createElement(`li`);
+    li.textContent = text;
+    getList.appendChild(li);
+}
+
+getForm.addEventListener(`submit`, function (e) {
+    e.preventDefault();
+
+    itemsArray.push(getItems.value);
+    localStorage.setItem(`item`, JSON.stringify(itemsArray));
+    liMaker(getItems.value);
+    getItems.value="";
+});
+
+addBtn.addEventListener(`click`, function () {
+
+    itemsArray.push(getItems.value);
+    localStorage.setItem(`item`, JSON.stringify(itemsArray));
+    liMaker(getItems.value);
+    getItems.value="";
+});
+
